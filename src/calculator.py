158d517471094fcce96e87b2b17c6786c1bee17e
Vojtěch Bláha <90373255+Vojd56@users.noreply.github.com>
2025-03-04 10:27:23 +0100

    dada

---
+++ src/calculator.py
@@ -0,0 +1,4 @@
+import calculator
+
+def add (a, b):
+    return a + b
---
+++ tests/test_calculator_parametrized.py
@@ -0,0 +1,15 @@
+
+import calculator
+
+@pytest.mark.paremetrize(
+    "a, b, expected",
+    [
+        (1, 2, 3),
+        (0, 5, 5),
+        (-2, 9, 7),
+        (5, -8, -3),
+    ],
+)
+
+def test_add_parametrized(a, b, expected):
+    assert calculator.add(a, b) == expected
